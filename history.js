1b8230aa8453e201de5830c6e91a7d0cb5356b45
Mahfuj-HSTU <rmahfujur16@gmail.com>
2022-09-15 00:14:18 +0600

    create blog html

--- history.js
+++ history.js
@@ -28,13 +28,11 @@ function displayHistory() {
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
     newRow.classList.add("card");
-
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <h3 class = "margin">${test.questionText}</h3>
+  <p class = "margin">You took: <span class="bold">${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
-
     histories.appendChild(newRow);
   });
 }
